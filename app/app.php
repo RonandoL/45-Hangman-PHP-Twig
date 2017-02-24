5a8b7fbd93bdd66e5b5c30fe6ba8e9367403abce
Ronando Long <ronando.long@gmail.com>
2017-02-24 13:25:49 -0800

    add ability to save array of game words and pick a random word from the array...I hope.

--- app/app.php
+++ app/app.php
@@ -3,12 +3,21 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/hangman.php";
 
-    session_start();                          // For global variable, saving in browser cache
-    if (empty($_SESSION['list_of_letters'])) {
-        $_SESSION['list_of_letters'] = array();
+    // 1) create $_Session & home route
+    // 2) create variable array of words to use & word that is randomly selected
+    // 3) create instantiated word variable for new word, randomly from words array
+    // 4) $app['debug'] = true .....
+
+    session_start();                   // session_start to have session remembered through entire site, all pages
+    if (empty($_SESSION['list_of_hangmans'])) {
+        $_SESSION['list_of_hangmans'] = array();
+        $gameWords = array('Batman', 'Robin', 'Superman');   // words the game will pick from
+        $gameWord = new Hangman($gameWords[rand(0, strlen($gameWords))]);  // Randomly picks word to play with and gets length of word
+        array_push($_SESSION['list_of_hangmans'], $gameWord);  // saving specific word in SESSION variable
     }
 
     $app = new Silex\Application();
+    $app['debug'] = true;    // I don't know why this is here
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
@@ -17,13 +26,16 @@
 
   // 1. Route for home page
     $app->get('/', function() use ($app) {
-        return $app['twig']->render('hangman.html.twig', array('letters' => Hangman::getAll()));
+        return $app['twig']->render('hangman.html.twig', array('theGame' => Hangman::getAll()));
     });
 
   // 2. Route for sending instantiated new object (new task) to /tasks URL
-    $app->post('/hangman', function() use ($app) {
-        
-    });
+    // $app->post('/hangman', function() use ($app) {
+    //     $letter = new Hangman($_POST['letter']);
+    //     $letter->save();
+    //
+    //     return $app['twig']->render('hangman.html.twig', array('theGame' => Hangman::getAll()));
+    // });
 
   // 3. Route for deleting all tasks
     // $app
--- src/hangman.php
+++ src/hangman.php
@@ -1,33 +1,80 @@
 <?php
     class Hangman
     {
-        private $letter;
-        private $words;
+        private $word;
+        private $guess_number;
+        private $letters_guessed;
+        private $hidden_word;
+        private $message;
 
-        function __costruct($letter)
+        function __costruct($new_word, $starting_guess = 0, $letters_guessed = array(), $hidden_word = array(), $message = "")
         {
-            $this->letter = $letter;
-            $this->words = $_SESSION['Batman', 'Robin', 'Superman'];
+            $this->word = $new_word;
+            $this->guess_number = $starting_guess;
+            $this->letters_guessed = $letters_guessed;
+            $this->hidden_word = $hidden_word;
+            $length = strlen($this->word);
+                for ($i = 0; $i < $length; $i++) {     // to display dashes for the hidden word!
+                  array_push($this->hidden_word, '_');
+                }
+            $this->message = $message;
         }
 
-        function getLetter()
+      // GETTERS
+        function getWord()
         {
-            return $this->letter;
+            return $this->word;
         }
 
-        function getWords()
+        function getGuessNumber()
         {
-            return $this->words;
+            return $this->guess_number;
         }
 
+        function getLettersGuessed()
+        {
+            return $this->letters_guessed;
+        }
+
+        function getHiddenWord()
+        {
+            return $this->hidden_word;
+        }
+
+        function getMessage()
+        {
+            return $this->message;
+        }
+
+      // FUNCTIONS
+        function checkWord($guess_word)
+        {
+            if (strtolower($this->word) == strtolower($guess_word)) {
+              return true;
+            } else {
+              return false;
+            }
+        }
+
+        function checkLetter($guessed_letter)
+        {                         // strpos() Finds position of 1st occurrence of a guessed_letter in word
+            if (strpos($this->word, strtolower($guessed_letter)) != false ) {
+              return true;
+            } else {
+              return false;
+            }
+        }
+
+
+        // GETALL & SAVE
         static function getAll()
         {
-            $_SESSION['list_of_letters'];
+            return $_SESSION['list_of_hangmans'];
         }
 
         function save()
         {
-            array_push($_SESSION['list_of_letters'], $letter);
+            array_push($_SESSION['list_of_hangmans'], $this);
         }
 
     }
